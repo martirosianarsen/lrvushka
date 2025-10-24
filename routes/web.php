1ece8750079cfbf7fa5015279077f7a7eda936af
Arsen Martirosian <martirosian.arsen@gmail.com>
2025-10-24 16:58:26 +0400

    next init

--- routes/web.php
+++ routes/web.php
@@ -6,30 +6,42 @@ use App\Livewire\Settings\Profile;
 use App\Livewire\Settings\TwoFactor;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
+use Livewire\Livewire;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'localize']
+], function () {
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('home');
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
+    Route::view('dashboard', 'dashboard')
+        ->middleware(['auth', 'verified'])
+        ->name('dashboard');
 
-    Route::get('settings/profile', Profile::class)->name('profile.edit');
-    Route::get('settings/password', Password::class)->name('user-password.edit');
-    Route::get('settings/appearance', Appearance::class)->name('appearance.edit');
+    Route::middleware(['auth'])->group(function () {
+        Route::redirect('settings', 'settings/profile');
 
-    Route::get('settings/two-factor', TwoFactor::class)
-        ->middleware(
-            when(
-                Features::canManageTwoFactorAuthentication()
+        Route::get('settings/profile', Profile::class)->name('profile.edit');
+        Route::get('settings/password', Password::class)->name('user-password.edit');
+        Route::get('settings/appearance', Appearance::class)->name('appearance.edit');
+
+        Route::get('settings/two-factor', TwoFactor::class)
+            ->middleware(
+                when(
+                    Features::canManageTwoFactorAuthentication()
                     && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
-                ['password.confirm'],
-                [],
-            ),
-        )
-        ->name('two-factor.show');
+                    ['password.confirm'],
+                    [],
+                ),
+            )
+            ->name('two-factor.show');
+
+        Livewire::setUpdateRoute(function ($handle) {
+            return Route::post('/livewire/update', $handle);
+        });
+    });
 });
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
         laravel({
+            publicDirectory: 'public_html',
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: true
         }),
-        tailwindcss(),
+        tailwindcss()
     ],
     server: {
-        cors: true,
-    },
-});
+        cors: true
+    }
+})
